c0cda0561d1cfcdae0772f5c1f3d7da1d83b7012
Ams272 <sadiqabdulmuhaymin@gmail.com>
2022-03-10 04:48:42 -0800

    cool

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,22 @@
 #include <stdio.h>
 
+/**
+ * main - explained
+ * Return: 0 returned
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int ln;
+	long long int lnln;
+	float fl;
+
+	printf("Size of a char: %lu\n", sizeof(c));
+	printf("Size of a int: %lu\n", sizeof(i));
+	printf("Size of a long int: %lu\n", sizeof(ln));
+	printf("Size of a long long int: %lu\n", sizeof(lnln));
+	printf("Size of a float: %lu\n", sizeof(fl));
+
+	return (0);
+}
