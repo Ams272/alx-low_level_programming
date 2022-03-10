d492837e4beeb3159924f1f77db9a2a9e7585436
Ams272 <sadiqabdulmuhaymin@gmail.com>
2022-03-10 06:08:05 -0800

    done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main doing well
+ * Return: 0 returns
+ */
+
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
+
