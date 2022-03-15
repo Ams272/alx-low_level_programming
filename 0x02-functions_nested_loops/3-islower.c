7d9f97f9cad110fc36814a7c6f3815f880691110
Ams272 <sadiqabdulmuhaymin@gmail.com>
2022-03-15 13:12:45 -0700

    do

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - lower case
+ * @c: para 1
+ *
+ * Return: 0 or 1
+ *
+ */
+
+int _islower(int c)
+{
+	if (c == islower(c))
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - alphabetic character
+ * @c: parameter
+ *
+ * Return: 0
+ *
+ *
+ */
+
+int isalpha(int);
+int _isalpha(int c)
+{
+	if (c == isalpha(c))
+	{
+		return (1);
+	}
+	return (0);
+}
