1d4b03e683297b5963a49f30d356573482287d78
Ams272 <sadiqabdulmuhaymin@gmail.com>
2022-03-17 04:49:09 -0700

    d:

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - numbers printed
+ * Return: void
+ */
+void print_numbers(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		printf("%d\n", i);
+	}
+}
