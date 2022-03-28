33ef229ab0766f4abc491931e7b967905c0fa024
Ams272 <sadiqabdulmuhaymin@gmail.com>
2022-03-28 05:27:30 -0700

    first

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memset - memset func
+ * @s: firsst arg
+ * @b: second arg
+ * @n: last arg
+ *
+ * Return: something
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (int n; n > 0; n--)
+	{
+		s[i] = b;
+		i++;
+		n--;
+	}
+	return(s);
+}
