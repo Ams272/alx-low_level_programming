873604a49132409d3c8e7ff83f35f153abcbb4e0
Ams272 <sadiqabdulmuhaymin@gmail.com>
2022-03-28 06:12:27 -0700

    all

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - locate string
+ * @s: string
+ * @c: charac
+ *
+ * Return: c or null
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0' ; i++)
+	{
+		if (s[i] == c)
+		return (&s[i]);
+	}
+	return (0);
+}
