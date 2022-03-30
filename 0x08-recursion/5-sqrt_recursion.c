a428307c3cec1c6e1f9acc2e714d2b7dfea109e5
Ams272 <sadiqabdulmuhaymin@gmail.com>
2022-03-30 04:50:08 -0700

    d

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,39 +1,28 @@
 #include "main.h"
+
 /**
- * sqtRecursive - computes square root recursively
- * @n: given number
- * @m: comparison number
- * Return: 1 if not found sqrroot, else sqrroot
- **/
-int sqtRecursive(int n, int m)
-{
-	if (n <= 0)
-		return (-1);
-	if (n * n == m)
-		return (n);
-	return (sqtRecursive(n - 1, m));
-}
-/**
- * _sqrt_recursion - finds the natural square root of a number
- * @n: given number
- * Return: square root of n or -1
- **/
+ * _sqrt_recursion - main funct
+ * @n: int n
+ * Return: int
+ */
 int _sqrt_recursion(int n)
 {
-	if (n == 1)
-		return (1);
-	return (sqtRecursive(n / 2, n));
+	return (_sqrt(n, 1));
 }
+
 /**
- * is_prime_number - checks if a given number is prime
- * @n: given number
- * Return: 1 if number is prime else 0
- **/
-int is_prime_number(int n)
+ * _sqrt - _sqrt_recursion
+ * @n: integer paramtr
+ * @i: integer parameter
+ * Return: sqrt
+ */
+int _sqrt(int n, int i)
 {
-	if (n <= 1 || _sqrt_recursion(n) >= 1)
-		return (0);
-	if (_sqrt_recursion(n) == -1)
-		return (1);
-	return (_sqrt_recursion(n));
+	if (n < 0)
+		return (-1);
+	if ((i * i) > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_sqrt(n, i + 1));
 }
