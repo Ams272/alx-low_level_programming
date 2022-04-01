42c9d46ea3d01204da680cd8d7a0c8e029536df9
Ams272 <sadiqabdulmuhaymin@gmail.com>
2022-04-01 04:34:03 -0700

    first

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlb.h>
+
+/**
+ * main - print name
+ * @argc: counter
+ * @argv: array
+ *
+ * Return: integer
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
